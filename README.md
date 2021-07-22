2fdef5a5f930655313e011f2ffb24e9837064380
doubledeft <58928569+doubledeft@users.noreply.github.com>
2021-07-22 16:18:45 +0800

    增加账号操作的二级目录

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Intrusion_detection_system_ZSY
+
+
+## 简单的使用方法：
+后端启动方法：终端进入server文件夹，py manage.py runserver
+创建超级管理员账号,py manage.py createsuperuser
+数据库生成，生成数据库定义语言，py manage.py makemigrations,生成本地数据库，py manage.py migrate
--- server/api/urls.py
+++ server/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-    path('change_password/', views.change_password),
+    path('log/login/', views.login, name='login'),
+    path('log/register/', views.register, name='register'),
+    path('log/change_password/', views.change_password, name='change_password'),
 ]
