a39cf5fa931a9dbbac49c86f43221b40580af82a
doubledeft <58928569+doubledeft@users.noreply.github.com>
2021-07-22 20:27:23 +0800

    增加 管理员接口

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('log/login/', views.login, name='login'),
     path('log/register/', views.register, name='register'),
     path('log/change_password/', views.change_password, name='change_password'),
+    path('log/add_admin/', views.add_admin, name='add_admin')
 ]
