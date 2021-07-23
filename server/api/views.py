6b0c0f64382aee035fe4b04a5c5186b0d7b2fbe8
doubledeft <58928569+doubledeft@users.noreply.github.com>
2021-07-23 15:14:16 +0800

    添加功能：更改用户名，查询所有用户，查询单独用户（返回用户拥有的摄像头信息）

--- server/api/views.py
+++ server/api/views.py
@@ -11,15 +11,46 @@ import datetime
 from django.core.serializers.json import DjangoJSONEncoder
 
 # 导入model中的User
-from .models import User
+from .models import User, Camera
+
+
+# 代码编码规则
+# 下划线命名法
+# 函数名 动作+对象，如更改密码 change——password
 
 
 # def test(request):
 #     return HttpResponse("result")
 
 
+# #函数模板
+# @csrf_exempt
+# def xxx(request):
+#     dic={}
+#     if request.method != 'POST':
+#         dic['status'] = "Failed"
+#         dic['message'] = "Wrong Method"
+#         return HttpResponse(json.dumps(dic))
+#     try:
+#         post_content = json.loads(request.body)
+#         xx = post_content['xx']
+#         user = User.objects.get(xx=xx)
+#     except(KeyError, json.decoder.JSONDecodeError):
+#         dic['status'] = "Failed"
+#         dic['message'] = "No Input"
+#         return HttpResponse(json.dumps(dic))
+#     except User.DoesNotExist:
+#         dic['status'] = "Failed"
+#         dic['message'] = "Wrong Username"
+#         return HttpResponse(json.dumps(dic))
+#
+#     dic['status'] = "Success"
+#     return HttpResponse(json.dumps(dic))
+
+
 # id permission username password
 # 登录验证内容 输入合理 存在账号  密码正确
+# 返回id
 @csrf_exempt
 def login(request):
     dic = {}
@@ -57,7 +88,7 @@ def login(request):
 
 # 注册账号
 # method POST
-# id permission username password
+# username password
 # 注册逻辑 存在输入 用户不存在进行注册
 # 要求：
 # 重复用户检测
@@ -89,6 +120,66 @@ def register(request):
         return HttpResponse(json.dumps(dic))
 
 
+# 注销账号
+# 提供id注销
+#   先检测摄像头存在，删除摄像头
+#   再删除账号
+@csrf_exempt
+def delete_account(request):
+    dic = {}
+    if request.method != 'POST':
+        dic['status'] = "Failed"
+        dic['message'] = 'Wrong Method'
+        return HttpResponse(json.dumps(dic))
+    try:
+        post_content = json.loads(request.body)
+        user_id = post_content['id']
+        user = User.objects.get(id=user_id)
+        camera = Camera.objects.filter(owner=user)
+        if len(camera) is not 0:
+            for c in camera:
+                c.delete()
+        user.delete()
+    except(KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = 'Failed'
+        dic['message'] = 'Wrong id'
+        return HttpResponse(json.dumps(dic))
+
+    dic['status'] = 'Success'
+    return HttpResponse(json.dumps(dic))
+
+
+# 更改用户名
+#   id正确
+#   更改后的用户名不存在
+@csrf_exempt
+def change_username(request):
+    dic = {}
+    if request.method == 'POST':
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Method"
+        return HttpResponse(json.dumps(dic))
+    try:
+        post_content = json.loads(request.body)
+        user_id = post_content['id']
+        user = User.objects.get(id=user_id)
+    except(KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Username"
+        return HttpResponse(json.dumps(dic))
+
+    dic['status'] = "Success"
+    return HttpResponse(json.dumps(dic))
+
+
 # 更改密码
 # 根据用户主键id更改
 @csrf_exempt
@@ -101,10 +192,14 @@ def change_password(request):
     try:
         post_content = json.loads(request.body)
         user_id = post_content['id']
-        password = post_content['password']
+        new_password = post_content['password']
         user = User.objects.get(id=user_id)
-        user.password = make_password(password)
+        user.password = make_password(new_password)
         user.save()
+    except(KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
     except User.DoesNotExist:
         dic['status'] = 'Failed'
         dic['message'] = 'Wrong id'
@@ -115,6 +210,79 @@ def change_password(request):
 
     # dic = {'message': "hello"}
     # return HttpResponse(json.dumps(dic))s
+
+
+# 查询所有的用户
+@csrf_exempt
+def query_all(request):
+    dic = {}
+    if request.method != 'POST':
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Method"
+        return HttpResponse(json.dumps(dic))
+    try:
+        post_content = json.loads(request.body)
+        permission = post_content['permission']
+        # user = User.objects.filter(permission=permission)
+        users = User.objects.filter(permission=permission)
+        array = []
+        for user in users:
+            dics = {'id': user.id, "username": user.username}
+            array.append(dics)
+        # print(array)
+    except(KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Username"
+        return HttpResponse(json.dumps(dic))
+
+    dic['status'] = "Success"
+    dic['user_list'] = array
+    # dic['id'] = list(user.values('id'))
+    # dic['username'] = list(user.values('username'))
+    return HttpResponse(json.dumps(dic))
+
+
+# 查询独立用户
+# 查询一个用户， 并返回用户所用的所有摄像头
+# 根据id 查询
+@csrf_exempt
+def query_user(request):
+    dic = {}
+    if request.method != 'POST':
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Method"
+        return HttpResponse(json.dumps(dic))
+    try:
+        post_content = json.loads(request.body)
+        user_id = post_content['id']
+        user = User.objects.get(id=user_id)
+        cameras = Camera.objects.filter(owner=user)  # filter不会触发摄像头
+        array = []
+        if len(cameras) is not 0:
+            for camera in cameras:
+                disc = {'id': camera.id, 'name': camera.name, 'url': camera.url, 'description': camera.description,
+                        'owner_username': user.username
+                        }
+                array.append(disc)
+            dic['status'] = "Success"
+            dic['camera_list'] = array
+        else:
+            dic['status'] = "Success"
+            dic['camera_list'] = 'No Exist Camera'
+    except(KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Username"
+        return HttpResponse(json.dumps(dic))
+
+    return HttpResponse(json.dumps(dic))
 
 
 # 添加管理员账号
